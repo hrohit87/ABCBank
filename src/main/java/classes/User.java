505c16e8ae5c630c441267ea71ff65cd345a0c96
rohith <rohith@siddhatech.com>
2012-11-27 11:05:25 +0530

    FIfth

--- src/main/java/classes/User.java
+++ src/main/java/classes/User.java
@@ -59,7 +59,6 @@ public class User implements Serializable,ValueChangeListener,ActionListener
     
     public String check()
     {
-        
          System.out.println("inside check "+username);
          HttpSession ss= (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true); 
          ss.setAttribute("USERNAME","rohit");
